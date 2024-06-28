246d2ff70d68078296ee42c4b567b4d9fc02bf3e
Alena Ageeva <alenaageevapv@gmail.com>
2024-06-28 17:13:42 -0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/concurrent/ConsoleProgress.java
@@ -0,0 +1,30 @@
+package concurrent;
+
+public class ConsoleProgress implements Runnable {
+    public static void main(String[] args) {
+        try {
+            Thread progress = new Thread(new ConsoleProgress());
+            progress.start();
+            Thread.sleep(5000); /* симулируем выполнение параллельной задачи в течение 5 секунд. */
+            progress.interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        int index = 0;
+        var process = new char[]{'-', '\\', '|', '/'};
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                index = index == 4 ? 0 : index;
+                System.out.print("\r load: " + process[index++]);
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+}
