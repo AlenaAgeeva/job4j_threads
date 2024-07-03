7bbb95495022d1d5751bc5251c7cf58eee4ffcbd
Alena Ageeva <alenaageevapv@gmail.com>
2024-07-03 12:03:16 -0300

    1. Синхронизация общих ресурсов. [#1096]

---
+++ src/main/java/concurrent/Cache.java
@@ -0,0 +1,12 @@
+package concurrent;
+
+public class Cache {
+    private static Cache cache;
+
+    public synchronized static Cache getInstance() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
