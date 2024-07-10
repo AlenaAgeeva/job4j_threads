2145d9e0efc953e3072261834643efd25135d83e
Alena Ageeva <alenaageevapv@gmail.com>
2024-07-09 21:52:52 -0300

    0. CAS - Operations [#6859]

---
+++ src/main/java/concurrent/CASCount.java
@@ -0,0 +1,23 @@
+package concurrent;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicInteger count = new AtomicInteger();
+
+    public void increment() {
+        int currentValue;
+        int nextValue;
+        do {
+            currentValue = get();
+            nextValue = currentValue + 1;
+        } while (!count.compareAndSet(currentValue, nextValue));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
