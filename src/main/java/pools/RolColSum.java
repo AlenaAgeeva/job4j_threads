3bfc6bc9402f25ee755318c21fcb36f830b5ed1a
Alena Ageeva <alenaageevapv@gmail.com>
2024-07-18 15:25:28 -0300

    4. CompletableFuture [#361626 #505547]

--- src/main/java/pools/RolColSum.java
+++ src/main/java/pools/RolColSum.java
@@ -3,26 +3,6 @@ package pools;
 import java.util.concurrent.CompletableFuture;
 
 public class RolColSum {
-    public static class Sums {
-        private int rowSum;
-        private int colSum;
-
-        public int getRowSum() {
-            return rowSum;
-        }
-
-        public void setRowSum(int rowSum) {
-            this.rowSum = rowSum;
-        }
-
-        public int getColSum() {
-            return colSum;
-        }
-
-        public void setColSum(int colSum) {
-            this.colSum = colSum;
-        }
-    }
 
     public static Sums[] sum(int[][] matrix) {
         Sums[] sums = new Sums[matrix.length];
--- src/test/java/pools/RolColSumTest.java
+++ src/test/java/pools/RolColSumTest.java
@@ -9,23 +9,22 @@ class RolColSumTest {
     @Test
     public void testSum() {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        RolColSum.Sums[] sums = RolColSum.sum(matrix);
-        assertThat(sums[0].getColSum()).isEqualTo(12);
-        assertThat(sums[0].getRowSum()).isEqualTo(6);
-        assertThat(sums[1].getColSum()).isEqualTo(15);
-        assertThat(sums[1].getRowSum()).isEqualTo(15);
-        assertThat(sums[2].getColSum()).isEqualTo(18);
-        assertThat(sums[2].getRowSum()).isEqualTo(24);
+        Sums[] expected = {
+                new Sums(6, 12),
+                new Sums(15, 15),
+                new Sums(24, 18)
+        };
+        assertThat(RolColSum.sum(matrix)).isEqualTo(expected);
     }
+
     @Test
     public void testAsyncSum() {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        RolColSum.Sums[] sums = RolColSum.asyncSum(matrix);
-        assertThat(sums[0].getColSum()).isEqualTo(12);
-        assertThat(sums[0].getRowSum()).isEqualTo(6);
-        assertThat(sums[1].getColSum()).isEqualTo(15);
-        assertThat(sums[1].getRowSum()).isEqualTo(15);
-        assertThat(sums[2].getColSum()).isEqualTo(18);
-        assertThat(sums[2].getRowSum()).isEqualTo(24);
+        Sums[] expected = {
+                new Sums(6, 12),
+                new Sums(15, 15),
+                new Sums(24, 18)
+        };
+        assertThat(RolColSum.asyncSum(matrix)).isEqualTo(expected);
     }
 }
